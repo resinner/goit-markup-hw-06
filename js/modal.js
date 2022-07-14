6938576ef2230a67228d7714e0554a3a973100fd
resinner <revengingsinner@gmail.com>
2022-07-14 13:26:35 +0300

    fix scrollbar

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector('[data-modal]'),
     body: document.querySelector('[data-body]'),
   };
@@ -13,5 +13,4 @@
     refs.modal.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
   }
-
 })();
